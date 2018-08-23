92066239909b0fb61d9b24a8264d7e684d385b20
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2018-08-23 17:09:59 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# goeip
+Single file library for [Kealper](https://github.com/Kealper)'s geoip utility written in Golang without extra dependencies
+
+# Installing
+`go get github.com/JoshuaDoes/goeip`
+
+# Example
+```go
+package main
+
+import "fmt"
+import "github.com/JoshuaDoes/goeip"
+
+func main() {
+	result, err := goeip.Lookup("joshuadoes.com")
+	if err != nil {
+		fmt.Println("Error: " + fmt.Sprintf("%v", err))
+		return
+	}
+	if result.Error > 0 {
+		fmt.Println("Lookup error: " + result.Details)
+	}
+
+	fmt.Println("City: " + result.City)
+	fmt.Println("State: " + result.State)
+}
+```
+### Output
+
+```
+> go run main.go
+City: Dallas
+State: Texas
+```
---
+++ goeip.go
@@ -0,0 +1,63 @@
+package goeip
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+//Result holds data returned by the API
+type Result struct {
+	Error      int
+	Details    string `json:"Detail"`
+	IPAddr     string
+	Hostname   string
+	City       string
+	State      string
+	PostalCode string
+	Country    Country
+	Timezone   string
+	Location   Location
+	ASN        ASN
+}
+
+//Country holds data about a country
+type Country struct {
+	Name string `json:"Name"`
+	Code string
+}
+
+//Location holds data about a location
+type Location struct {
+	Latitude  float64
+	Longitude float64
+}
+
+//ASN holds data about an ASN
+type ASN struct {
+	Number       int
+	Organization string
+}
+
+//Lookup performs a GET request to the API and returns data about the specified hostname/ipaddr
+//The returned error only occurs for internal library errors, please check to see if *Result.Error is greater than 0 and use *Result.Details if so
+func Lookup(host string) (*Result, error) {
+	escapedHost := url.QueryEscape(host)
+
+	url := fmt.Sprintf("https://kealper.com/util/geoip/api.cgi?type=json&ip=%s", escapedHost)
+
+	data := &Result{}
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	err = unmarshal(res, data)
+
+	return data, err
+}
+
+func unmarshal(body *http.Response, target interface{}) error {
+	defer body.Body.Close()
+	return json.NewDecoder(body.Body).Decode(target)
+}
